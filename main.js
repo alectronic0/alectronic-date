69d1615f9b52b7339851f79d5f0da4620548c9b7
Alec <alec@alectronic.co>
2026-07-16 23:07:41 +0100

    cookie stuff

--- main.js
+++ main.js
@@ -837,7 +837,8 @@
         };
         const footerLinks = [
             ...(C.contact.links || []),
-            { label: 'Cookie Settings', icon: '🍪', href: 'javascript:Cookiebot.renew()' }
+            { label: 'Cookie Settings', icon: '🍪', href: 'javascript:Cookiebot.renew()' },
+            { label: 'Cookie Vault', icon: '🍪', href: 'cookies.html' }
         ];
         setHtml('[data-footer="links"]', footerLinks.map(linkHtml).join(''));
     }
